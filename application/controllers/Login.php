ce9a1ea56bce1c2e4d967b274256b35a227388e5
David Alfa Sunarna <davidalfasunarna@gmail.com>
2017-09-23 17:44:40 +0700

    Fixing Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,11 +8,40 @@ class Login extends MEET_Controller
 
             return redirect('newsfeed', 'refresh');
         }
-        if ($this->session->userdata('user_id')) {
-            return redirect('newsfeed', 'refresh');
-        }
+
         $this->load->view('login', array('title' => 'Login to Meet', 'page_class' => 'login'));
     }
+
+    public function try_login()
+    {
+        $username = $this->input->post('username');
+        $password = md5($this->input->post('password'));
+
+        if($this->Functions->check_login($username,$password))
+        {
+            $id = $this->Functions->get_user_id_from_username($username);
+
+            $status = $this->Functions->check_active_user($id);
+
+            if ($status == 1) 
+            {
+                $this->session->set_userdata('user_id', $id);
+
+                return redirect('', 'refresh');
+            }
+            else
+            {
+                $this->session->set_flashdata('unactive_user', 'Please activate your account');
+                return redirect('');
+            }
+        }
+        else
+        {
+            $this->session->set_flashdata('failed_login', 'Invalid username or password');
+            return redirect('');
+        }
+    }
+
     public function logout() {
         $this->session->unset_userdata('user_id');
 
--- includes/js/Ajax.js
+++ includes/js/Ajax.js
@@ -1,33 +1,34 @@
 function check_login() {
-    var username = $('#log_username_field').val();
-    var pass = $('#log_pass_field').val();
-    if (!username && !pass) {
-        return false;
-    }
-    var data = {
-        'login': username,
-        'pass': pass,
-        'cb_csrf_secured': $('#cb_csrf_secured').val()
-    }
-    var uri = BASE_URL + "ajax/login";
-    var request = $.ajax({
-        type: 'POST',
-        url: uri,
-        data: data,
-        dataType: 'json',
-        encode: true
-    }).done(function(data, e) {
-        $('#cb_csrf_secured').val(data.cb_csrf_secured);
-        if (data.error) {
-            $('p#login_errors').text(data.error);
-            e.preventDefault();
-            return false;
-        } else {
-            location.reload();
-            e.preventDefault();
-            return false;
-        }
-    });
+    //console.log("wew");
+    // var username = $('#log_username_field').val();
+    // var pass = $('#log_pass_field').val();
+    // if (!username && !pass) {
+    //     return false;
+    // }
+    // var data = {
+    //     'login': username,
+    //     'pass': pass,
+    //     'cb_csrf_secured': $('#cb_csrf_secured').val()
+    // }
+    // var uri = BASE_URL + "ajax/login";
+    // var request = $.ajax({
+    //     type: 'POST',
+    //     url: uri,
+    //     data: data,
+    //     dataType: 'json',
+    //     encode: true
+    // }).done(function(data, e) {
+    //     $('#cb_csrf_secured').val(data.cb_csrf_secured);
+    //     if (data.error) {
+    //         $('p#login_errors').text(data.error);
+    //         e.preventDefault();
+    //         return false;
+    //     } else {
+    //         location.reload();
+    //         e.preventDefault();
+    //         return false;
+    //     }
+    // });
 };
 
 function create_post() {
