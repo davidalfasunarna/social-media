5fb1066b588565676442a01d88816de014662011
David Alfa Sunarna <davidalfasunarna@gmail.com>
2017-09-23 20:06:27 +0700

    Update

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="<? base_url('/'); ?>">RETRO</a> </div>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="<?=base_url('');?>">RETRO</a> </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#" data-toggle="modal" data-target="#postModal"><img src="<?= base_url("includes/img/icons/new.png") ?>" alt="new post" class="img-responsive" width="30px"></a></li>
+                    <li style="background-color: #FFFCE6;"><a href="#" data-toggle="modal" data-target="#postModal"><img src="<?= base_url("includes/img/icons/new.png") ?>" alt="new post" class="img-responsive" width="30px"></a></li>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Find Friends</a></li>
                     <li><a href="#">Notifications</a></li>
